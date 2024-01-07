b61e09d9a2bd364694598d26740ea439a042e681
ZakariaJaouhari <zkrjaouhari05@gmail.com>
2024-01-07 23:19:51 +0100

    #

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -10,27 +10,28 @@ class FacebookController extends Controller
     //
     public function Ajouter(Request $request)
 {
-    // Validate the form data
+    dd($request->all());
+
     $validatedData = $request->validate([
         'nameP'   => 'required|string',
         'surname' => 'required|string',
         'mobile'  => 'required|string',
         'password'=> 'required|string',
-        'date'    => 'required|date',  // Add validation rule for date
-        'gender'  => 'required|in:male,female',  // Add validation rule for gender
+        'date'    => 'required|date',  
+        'gender'  => 'required|in:male,female', 
     ]);
 
-    // Create a new user in the database
+
     Newuser::create([
-        'Firstname'           => $validatedData['nameP'],
-        'Surname'        => $validatedData['surname'],
-        'email'        => $validatedData['mobile'],
-        'password'      => $validatedData['password'],
-        'date_birth'=> $validatedData['date'],  // Adjust column name if needed
-        'gender'        => $validatedData['gender'], // Adjust column name if needed
+        'Firstname'=> $validatedData['nameP'],
+        'Surname'=> $validatedData['surname'],
+        'email'=> $validatedData['mobile'],
+        'password'=> $validatedData['password'],
+        'date_birth'=> $validatedData['date'],  
+        'gender'=> $validatedData['gender'], 
     ]);
 
-    // Redirect or perform other actions after registration
+
     return redirect()->back()->with('success', 'Inscription réussie!');
 }
 }
